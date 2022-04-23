80cdb2fa71fc100f3db1c06b30389571cd323a51
chuckk589 <crazy-rabit@yandex.ru>
2022-04-24 00:15:50 +0300

    comm

--- src/bot/common/keyboards.ts
+++ src/bot/common/keyboards.ts
@@ -1,5 +1,6 @@
 import { Keyboard } from "grammy";
 import { InlineKeyboardMarkup } from "grammy/out/platform.node";
+import { Offers } from "src/mikroorm/entities/Offers";
 import { BotContext, OfferMode } from "src/types/interfaces";
 import i18n from "../middleware/i18n";
 
@@ -36,4 +37,24 @@ export const manageOfferMenu = (offerId: number, locale: string, mode: OfferMode
             ]
         ]
     }
+}
+export const manageExistingOfferMenu = (offer: Offers, ctx: BotContext): InlineKeyboardMarkup => {
+    const status = offer.offerStatus.value
+    //const initiator = offer.initiator.chat_id === ctx.from.id
+    return {
+        inline_keyboard: [
+            [
+                { callback_data: '2', text: ctx.i18n.t('setWallet') }
+            ]
+        ]
+    }
+    // return Markup.inlineKeyboard([
+    //     [Markup.callbackButton(ctx.i18n.t('setWallet'), `setWallet`)],
+    //     status === 'pending' && initiator ? [Markup.callbackButton(ctx.i18n.t('sendOffer'), `sendOffer`)] : [],
+    //     isSeller && status === 'payed' ? [Markup.callbackButton(ctx.i18n.t('confirmShipping'), `confirmShipping`)] : [],
+    //     isSeller && offer.sellerWalletData && status === 'arrived' ? [Markup.callbackButton(ctx.i18n.t('getPayout'), `getPayout`)] : [],
+    //     !isSeller && status === 'shipped' ? [Markup.callbackButton(ctx.i18n.t('confirmArrival'), `confirmArrival`)] : [],
+    //     status !== 'closed' && status !== 'arbitrary' && status !== 'pending' ? [Markup.callbackButton(ctx.i18n.t('openArbitrary'), `openArbitrary`)] : [],
+    //     offer.offerStatus.id > 5 ? [Markup.callbackButton(ctx.i18n.t('leaveFeedback'), `leaveFeedback`)] : [],
+    // ]).extra()
 }

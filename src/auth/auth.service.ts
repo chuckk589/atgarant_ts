60d4fa25f41cad1e985c9bda0bce564bb0c8e20b
chuckk589 <crazy-rabit@yandex.ru>
2022-04-28 20:58:06 +0300

    daily

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,28 @@
+import { EntityManager } from '@mikro-orm/core';
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { Users } from 'src/mikroorm/entities/Users';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly em: EntityManager,
+  ) { }
+
+  async validateUser(username: string, pass: string): Promise<any> {
+    const user = await this.em.findOne(Users, username);
+    if (user && user.password === pass) {
+      const { password, ...result } = user;
+      return result;
+    }
+    return null;
+  }
+
+  async login(user: any) {
+    const payload = { username: user.username, sub: user.userId };
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
+  }
+}
